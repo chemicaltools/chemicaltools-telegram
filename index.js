0bda9531ae167a57cfe3bd63703f37fdf9881128
Jinzhe Zeng <njzjz@qq.com>
2019-03-11 23:51:44 +0800

    update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
 const chemicaltoolsbot = require('chemicaltools-bot')
 const token = '621653660:AAH-j39YYtzPgkueko9kSi4xIn4xnIlP1Mk';
-const bot = new TelegramBot(token, {polling: true});
+const options = {
+    webHook: {
+      port: 443
+    }
+  };
+const url = 'chemicaltoolstelegram.njzjz.win';
+const bot = new TelegramBot(token, options);
+bot.setWebHook(`${url}/bot${TOKEN}`);
 bot.onText(/(.+)/, (msg, match) => {
   const chatId = msg.chat.id;
   const resp = chemicaltoolsbot(match[0]); 
