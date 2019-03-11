2ff2a99735b07e3ba7edd2312a02359928e94888
Jinzhe Zeng <njzjz@qq.com>
2019-03-12 02:02:03 +0800

    update

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
 const TelegramBot = require('node-telegram-bot-api');
 const chemicaltoolsbot = require('chemicaltools-bot')
-const express = require('express');
-const bodyParser = require('body-parser');
 const TOKEN = '621653660:AAH-j39YYtzPgkueko9kSi4xIn4xnIlP1Mk';
-const url = 'chemicaltoolstelegram.njzjz.win';
-const bot = new TelegramBot(TOKEN);
-bot.setWebHook(`${url}/`);
+const bot = new TelegramBot(TOKEN, { polling: true });
 bot.onText(/(.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = chemicaltoolsbot(match[0]); 
-  bot.sendMessage(chatId, resp);
+    const chatId = msg.chat.id;
+    const resp = chemicaltoolsbot(match[0]);
+    bot.sendMessage(chatId, resp);
 });
-const app = express();
-app.use(bodyParser.json());
-app.post(`/`, (req, res) => {
-  bot.processUpdate(req.body);
-  res.sendStatus(200);
-});
-module.exports = app
